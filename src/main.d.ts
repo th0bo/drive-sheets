dde982b5f80f6c3fc1b8816c4ece00e1f48b2fad
Théo Boulaire <th.boulaire@gmail.com>
2024-04-03 11:00:28 +0200

    Check boxes are managed as boolean

--- src/main.d.ts
+++ src/main.d.ts
@@ -1,10 +1,12 @@
 type FileParams = { fileName: string } | { fileId: string };
 
-type SaveParams = FileParams & { values: string[][], range: string };
+type CellValue = string | boolean;
+
+type SaveParams = FileParams & { values: CellValue[][], range: string };
 type Save = (params: SaveParams) => void
 
 type LoadParams = FileParams & { range: string };
-type Load = (params: LoadParams) => string[][];
+type Load = (params: LoadParams) => CellValue[][];
 
 type DriveSheetsLibrary = {
   save: Save;
